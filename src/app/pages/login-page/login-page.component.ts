78c17272eb92fdbf0beb57b39c2b92f8e00c23fc
Sean Maverick Saligue <skymavericksaligue@gmail.com>
2025-03-10 23:40:28 +0800

    FE-LP-1: Create Login Page

---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.less',
+})
+export class LoginPageComponent { }
